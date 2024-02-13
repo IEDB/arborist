57ff44d4b13d7a8a853d1694ee7a5f179963aff3
Daniel Marrama <danielmarrama@gmail.com>
2024-02-12 20:39:54 -0800

    add additional columns for source table sql query

--- src/iedb/peptide_source.sql
+++ src/iedb/peptide_source.sql
@@ -1,6 +1,10 @@
 SELECT DISTINCT
+  source_id AS 'Source ID',
   accession AS 'Accession',
+  database AS 'Database',
   name AS 'Name',
+  aliases AS 'Aliases',
+  synonyms AS 'Synonyms',
   sequence AS 'Sequence',
   organism_id AS 'Organism ID'
 FROM source
