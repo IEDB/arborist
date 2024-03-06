59b294235a14dc0c76cea38db610a864c607d45a
Daniel Marrama <dmarrama@lji.org>
2024-03-06 12:54:37 -0800

    generate leidos tables in combine script

--- src/protein_tree/protein_tree/combine_assignments.py
+++ src/protein_tree/protein_tree/combine_assignments.py
@@ -4,27 +4,14 @@ import argparse
 import pandas as pd
 from pathlib import Path
 
+import warnings
+warnings.filterwarnings('ignore')
 
-def main():
-  """Combine all peptide and peptide source assignments into two TSV files."""
-  
-  parser = argparse.ArgumentParser()
 
-  parser.add_argument(
-    'build_path',
-    type=str,
-    default='build/',
-    help='Path to species directory.'
-  )
-
-  args = parser.parse_args()
-  build_path = Path(args.build_path)
-  species_path = build_path / 'species'
-
-  print('Combining all peptide and source assignments...\n')
-
+def combine_assignments(build_path):
   all_peptides_df, all_sources_df = pd.DataFrame(), pd.DataFrame()
   
+  species_path = build_path / 'species'
   for species in species_path.iterdir():
     try:
       peptides_df = pd.read_csv(species / 'peptide-assignments.tsv', sep='\t')
@@ -35,6 +22,7 @@ def main():
       continue
     
     proteome['Sequence Length'] = proteome['Sequence'].apply(len)
+    id_to_seq_map = proteome.set_index('Protein ID')['Sequence'].to_dict()
     id_to_len_map = proteome.set_index('Protein ID')['Sequence Length'].to_dict()
 
     peptides_df['Species Taxon ID'] = species.name
@@ -44,6 +32,7 @@ def main():
     sources_df['Species Name'] = species_data['Species Name'].iloc[0]
     sources_df['Proteome ID'] = species_data['Proteome ID'].iloc[0]
     sources_df['Proteome Label'] = species_data['Proteome Taxon'].iloc[0]
+    sources_df['Parent Sequence'] = sources_df['Assigned Protein ID'].map(id_to_seq_map)
     sources_df['Parent Sequence Length'] = sources_df['Assigned Protein ID'].map(id_to_len_map)
 
     all_peptides_df = pd.concat([all_peptides_df, peptides_df])
@@ -75,6 +64,67 @@ def main():
 
   print(f'Peptide success rate: {peptide_success_count / peptide_count * 100:.2f}%')
   print(f'Source success rate: {source_success_count / source_count * 100:.2f}%')
+
+
+def generate_leidos_tables():
+  def protein_strategy(row):
+    if pd.notnull(row['ARC Assignment']):
+      return 'antigen-receptor-classifier'
+    elif row['Assignment Score'] > 80:
+      return 'strong-blast-match'
+    elif row['Assignment Score'] <= 80:
+      return 'weak-blast-match'
+    else:
+      return 'manual'
+
+  all_sources = pd.read_csv('build/arborist/all-source-assignments.tsv', sep='\t')
+  all_sources['Protein Strategy'] = all_sources.apply(protein_strategy, axis=1)
+  all_sources.loc[all_sources['Assigned Protein ID'].notnull(), 'Parent IRI'] = all_sources['Assigned Protein ID'].apply(lambda x: f'http://www.uniprot.org/uniprot/{x}')
+  all_sources.loc[all_sources['Assigned Protein ID'].notnull(), 'Parent Protein Database'] = 'UniProt'
+
+  source_parents = all_sources[[
+    'Source ID', 'Accession', 'Database', 'Name', 'Aliases', 'Synonyms', 'Organism ID', 
+    'Species Taxon ID', 'Species Name', 'Proteome ID', 'Proteome Label', 'Protein Strategy', 
+    'Parent IRI', 'Parent Protein Database', 'Assigned Protein ID', 'Parent Sequence Length'
+  ]]
+  source_parents.rename(columns={
+    'Organism ID': 'Taxon ID',
+    'Species Taxon ID': 'Species ID',
+    'Species Name': 'Species Label',
+    'Assigned Protein ID': 'Parent Protein Accession',
+  }, inplace=True)
+
+  parent_proteins = all_sources[[
+    'Assigned Protein ID', 'Parent Protein Database', 'Assigned Protein Name',
+    'Proteome ID', 'Proteome Label', 'Parent Sequence'
+  ]]
+  parent_proteins.rename(columns={
+    'Assigned Protein ID': 'Accession',
+    'Parent Protein Database': 'Database',
+    'Assigned Protein Name': 'Name',
+    'Parent Sequence': 'Sequence'
+  }, inplace=True)
+  parent_proteins.drop_duplicates(subset=['Accession'], inplace=True)
+
+  source_parents.to_csv('build/arborist/source-parents.tsv', sep='\t', index=False)
+  parent_proteins.to_csv('build/arborist/parent-proteins.tsv', sep='\t', index=False)
+
+def main():  
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument(
+    'build_path',
+    type=str,
+    default='build/',
+    help='Path to species directory.'
+  )
+
+  args = parser.parse_args()
+  build_path = Path(args.build_path)
+
+  print('Combining all peptide and source assignments...\n')
+  combine_assignments(build_path)
+  generate_leidos_tables()
 
 
 if __name__ == '__main__':
