863cb94c7096cfe1779180539b976c9f0ace0562
Daniel Marrama <danielmarrama@gmail.com>
2024-03-25 14:51:29 -0700

    fix epitope related object query in peptide.sql

--- src/iedb/peptide.sql
+++ src/iedb/peptide.sql
@@ -6,39 +6,28 @@ SELECT DISTINCT
   object.mol2_accession AS 'Source Accession',
   object.mol2_name AS 'Source Name',
   object.organism2_id AS 'Organism ID'
-FROM epitope, object
-WHERE epitope.e_object_id = object.object_id
-  AND object.mol1_seq IS NOT NULL
-  AND object.object_sub_type IN 
-      ('Peptide from protein', 'Discontinuous protein residues')
+FROM
+  object
+JOIN
+  epitope ON epitope.e_object_id = object.object_id
+WHERE
+  object.mol1_seq IS NOT NULL
+  AND object.object_sub_type IN ('Peptide from protein', 'Discontinuous protein residues')
 
 UNION
 
 SELECT DISTINCT
   epitope.epitope_id AS 'Epitope ID',
-  object.region AS 'Sequence',
+  object.mol1_seq AS 'Sequence',
   object.starting_position AS 'Starting Position',
   object.ending_position AS 'Ending Position',
   object.mol2_accession AS 'Source Accession',
   object.mol2_name AS 'Source Name',
   object.organism2_id AS 'Organism ID'
-FROM epitope, object
-WHERE epitope.related_object_id = object.object_id
-  AND object.region IS NOT NULL
-  AND object.object_sub_type IN 
-      ('Peptide from protein', 'Discontinuous protein residues')
-
-UNION
-
-SELECT DISTINCT
-  epitope.epitope_id AS 'Epitope ID',
-  object.region AS 'Sequence',
-  object.starting_position AS 'Starting Position',
-  object.ending_position AS 'Ending Position',
-  object.mol2_accession AS 'Source Accession',
-  object.mol2_name AS 'Source Name',
-  object.organism2_id AS 'Organism ID'
-FROM epitope, object
-WHERE epitope.related_object_id = object.object_id
-  AND object.mol1_seq IS NOT NULL
-  AND object.object_sub_type IN ('Protein')
+FROM
+  object
+JOIN
+  epitope ON epitope.related_object_id = object.object_id
+WHERE
+  object.mol1_seq IS NOT NULL
+  AND object.object_sub_type IN ('Peptide from protein', 'Discontinuous protein residues')
