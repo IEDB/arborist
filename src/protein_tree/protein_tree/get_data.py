d08d2aa9b524a6272e17f6dc33340fad7645d696
Daniel Marrama <danielmarrama@gmail.com>
2024-06-07 17:31:35 -0700

    protein tree: get_data.py formatting

--- src/protein_tree/protein_tree/get_data.py
+++ src/protein_tree/protein_tree/get_data.py
@@ -10,17 +10,15 @@ class DataFetcher:
     self.build_path = build_path
   
   def get_all_peptides(self) -> pd.DataFrame:
-    """Get all peptides from the written file."""
+    """Get all peptides from the written IEDB file."""
     return pd.read_csv(self.build_path / 'iedb' / 'peptide.tsv', sep='\t')
   
   def get_all_sources(self) -> pd.DataFrame:
-    """Get all peptide sources from the written file."""
+    """Get all peptide sources from the written IEDB file."""
     return pd.read_csv(self.build_path / 'iedb' / 'peptide_source.tsv', sep='\t')
 
-  def get_peptides_for_species(
-    self, all_peptides: pd.DataFrame, all_taxa: list
-  ) -> pd.DataFrame:
-    """Get peptides from the written file only for a specific species.
+  def get_peptides_for_species(self, all_peptides: pd.DataFrame, all_taxa: list) -> pd.DataFrame:
+    """Get peptides from the written IEDB file only for a specific species.
     
     Args:
       all_peptides: list of all peptides from the backend. 
@@ -28,14 +26,12 @@ class DataFetcher:
     """
     return all_peptides[all_peptides['Organism ID'].isin(all_taxa)]
 
-  def get_sources_for_species(
-    self, all_sources: pd.DataFrame, accessions: list
-  ) -> pd.DataFrame:
-    """Get peptide sources from the written file only for a specific species.
+  def get_sources_for_species(self, all_sources: pd.DataFrame, accessions: list) -> pd.DataFrame:
+    """Get peptide sources from the written IEDB file only for a specific species.
     
     Args:
       all_sources: list of all peptide sources from the backend.
-      all_taxa: list of all active children taxa for a species.
+      accessions: list of all source accessions for a species.
     """
     return all_sources[all_sources['Source Accession'].isin(accessions)]
 
