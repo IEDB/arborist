b54af2374e1e25e995b77ccc5c401348a54808ea
Daniel Marrama <danielmarrama@gmail.com>
2024-06-08 14:38:52 -0700

    source processor complete

--- src/protein_tree/protein_tree/assign2.py
+++ src/protein_tree/protein_tree/assign2.py
@@ -2,7 +2,7 @@ import argparse
 import subprocess
 import polars as pl
 from pathlib import Path
-
+from ARC.classifier import SeqClassifier
 from protein_tree.data_fetch import DataFetcher
 
 
@@ -18,9 +18,9 @@ class AssignmentHandler:
 
   def process_species(self):
     source_processor = SourceProcessor(self.taxon_id, self.group, self.sources, self.num_threads, self.species_path)
-    source_processor.process()
+    source_assignments = source_processor.process()
 
-    peptide_processor = PeptideProcessor()
+    peptide_processor = PeptideProcessor(source_assignments)
     peptide_processor.process()
 
   def cleanup_files(self):
@@ -41,54 +41,56 @@ class SourceProcessor:
 
   def process(self):
     self.write_source_data()
-    self.write_isoforms_to_fasta()
-
-    if self.group == 'vertebrate':
-      self.run_arc()
-      # arc_results = pd.read_csv(self.species_path / 'arc-results.tsv', sep='\t')
+    self.write_sources_to_fasta(self.sources)
 
     if self.run_mmseqs2:
-      self.mmseqs2_isoforms()
+      self.mmseqs2_sources()
     else:
-      self.blast_isoforms()
+      self.blast_sources()
 
-    top_proteins = self.select_top_protein()
+    top_proteins = self.select_top_proteins()
     top_protein_data = self.get_protein_data(top_proteins)
+
+    if self.group == 'vertebrate':
+      arc_df = self.run_arc()
+      source_assignments = self.combine_arc_data(top_protein_data, arc_df)
+    else:
+      source_assignments = top_protein_data.with_columns(
+        pl.lit(None).alias('ARC Assignment')
+      )
+    return source_assignments
 
   def write_source_data(self):
     self.sources.write_csv(self.species_path / 'source-data.tsv', separator='\t')
 
-  def write_isoforms_to_fasta(self):
+  def write_sources_to_fasta(self, df):
     with open(self.fasta_path, 'w') as fasta_file:
-      for row in self.sources.iter_rows(named=True):
+      for row in df.iter_rows(named=True):
         fasta_file.write(f">{row['Source Accession']}\n{row['Sequence']}\n")
 
-  def run_arc(self):
-    pass
-
-  def blast_isoforms(self):
+  def blast_sources(self):
     makeblastdb_path = self.bin_path / 'makeblastdb'
     blastp_path = self.bin_path / 'blastp'
 
     makeblastdb_cmd = [
-        str(makeblastdb_path), 
-        '-in', str(self.proteome_path), 
-        '-dbtype', 'prot', 
-        '-out', str(self.proteome_path)
+      str(makeblastdb_path), 
+      '-in', str(self.proteome_path), 
+      '-dbtype', 'prot', 
+      '-out', str(self.proteome_path)
     ]
     blastp_cmd = [
-        str(blastp_path), 
-        '-query', str(self.fasta_path), 
-        '-db', str(self.proteome_path), 
-        '-evalue', '1', 
-        '-outfmt', '10', 
-        '-num_threads', str(self.num_threads), 
-        '-out', str(self.species_path / 'alignments.csv')
+      str(blastp_path), 
+      '-query', str(self.fasta_path), 
+      '-db', str(self.proteome_path), 
+      '-evalue', '1', 
+      '-outfmt', '10', 
+      '-num_threads', str(self.num_threads), 
+      '-out', str(self.species_path / 'alignments.csv')
     ]
     subprocess.run(makeblastdb_cmd, check=True)
     subprocess.run(blastp_cmd, check=True)
 
-  def mmseqs2_isoforms(self):
+  def mmseqs2_sources(self):
     mmseqs2_path = self.bin_path / 'mmseqs'
 
     mmseqs2_cmd = [
@@ -101,37 +103,85 @@ class SourceProcessor:
     ]
     subprocess.run(mmseqs2_cmd, check=True)
 
-  def select_top_protein(self):
+  def run_arc(self):
+    arc_results_exist = (self.species_path / 'arc-results.tsv').exists()
+    if arc_results_exist:
+      old_arc_df = pl.read_csv(self.species_path / 'arc-results.tsv', separator='\t')
+      new_arc_sources = self.sources.filter(
+        ~pl.col('Source Accession').is_in(old_arc_df['id'])
+      )
+      if new_arc_sources.shape[0] == 0:
+        return old_arc_df
+      self.write_sources_to_fasta(new_arc_sources)
+
+    temp_results_path = self.species_path / 'arc-temp-results.tsv'
+    SeqClassifier(
+      outfile=str(temp_results_path),
+      threads=self.num_threads,
+      blast_path=str(self.bin_path)+'/'
+    ).classify_seqfile(f'{self.species_path}/sources.fasta')
+
+    temp_results = pl.read_csv(temp_results_path, separator='\t')
+    if arc_results_exist:
+      arc_df = pl.concat([old_arc_df, temp_results])
+    else:
+      arc_df = temp_results
+
+    arc_df.write_csv(self.species_path / 'arc-results.tsv', separator='\t')
+
+    return arc_df
+  
+  def combine_arc_data(self, top_protein_data, arc_df):
+    arc_df = arc_df.with_columns(
+      pl.concat_str(
+        [pl.col('class'), pl.col('chain_type'), pl.col('calc_mhc_allele')], separator='_', ignore_nulls=True
+      ).alias('ARC Assignment')
+    )
+    arc_df = arc_df.select('id', 'ARC Assignment')
+    top_protein_data = top_protein_data.join(
+      arc_df, how='left', left_on='Source Accession', right_on='id', coalesce=True
+    )
+    return top_protein_data
+
+  def select_top_proteins(self):
     alignment_cols = [
       'Query', 'Subject', '% Identity', 'Alignment Length', 'Mismatches', 'Gap Openings', 
       'Query Start', 'Query End', 'Subject Start', 'Subject End', 'E-value', 'Bit Score'
     ]
     if self.run_mmseqs2:
       alignments = pl.read_csv(self.species_path / 'alignments.tsv', separator='\t', has_header=False, new_columns=alignment_cols)
-      alignments['% Identity'] = alignments['% Identity'] * 100
+      alignments = alignments.with_columns(pl.col('% Identity').mul(100).alias('% Identity'))
     else:
       alignments = pl.read_csv(self.species_path / 'alignments.csv', separator=',', has_header=False, new_columns=alignment_cols)
       alignments = alignments.with_columns(pl.col('Subject').str.split('|').list.get(1))
 
     query_length_map = dict(self.sources.select('Source Accession', 'Length').iter_rows())
     alignments = alignments.with_columns(
-      pl.col('Query').map_dict(query_length_map).alias('Query Length')
+      pl.col('Query').replace(query_length_map).cast(pl.Int32).alias('Query Length')
     )
     alignments = alignments.with_columns(
       pl.col('Alignment Length').mul(pl.col('% Identity')).truediv(pl.col('Query Length')).alias('Score')
     )
-    return alignments.groupby('Query').agg(pl.all().sort_by('Score').last())
+    return alignments.group_by('Query').agg(pl.all().sort_by('Score').last())
 
   def get_protein_data(self, top_proteins):
     proteome = pl.read_csv(self.species_path / 'proteome.tsv', separator='\t')
-    protein_data = top_proteins.join(proteome, how='left', left_on='Subject', right_on='Protein ID')
-    print(protein_data)
+    protein_data = top_proteins.join(
+      proteome, how='left', left_on='Subject', right_on='Protein ID', coalesce=False
+    )
+    protein_data = protein_data.select(pl.col(
+      'Query', 'Score', 'Gene', 'Protein ID', 'Protein Name'
+    )).rename({
+      'Query': 'Source Accession', 'Score': 'Source Alignment Score', 'Gene': 'Source Assigned Gene',
+      'Protein ID': 'Source Assigned Protein ID', 'Protein Name': 'Source Assigned Protein Name'}
+    )
+    return protein_data
 
 
 
 class PeptideProcessor:
-  def __init__(self):
-    pass
+  def __init__(self, source_assignments):
+    self.source_assignments = source_assignments
 
   def process(self):
     pass
