ca201218836fc8cf8e78079f415f565da0e87ec9
Daniel Marrama <danielmarrama@gmail.com>
2024-06-13 16:09:43 -0700

    blast with correct cols for mapping calc

--- src/protein/protein_tree/leidos.py
+++ src/protein/protein_tree/leidos.py
@@ -73,64 +73,68 @@ def make_parent_proteins(all_parent_data):
     'Parent Protein ID', 'Parent Protein Database', 'Parent Protein Name', 'Assigned Protein Name',
     'Proteome ID', 'Proteome Label', 'Assigned Protein Sequence'
   ).rename({
-    'Parent Protein ID': 'Accession', 'Parent Protein Database': 'Database',
-    'Assigned Protein Name': 'Title', 'Assigned Protein Sequence': 'Sequence'
+    'Parent Protein ID': 'Accession', 'Parent Protein Database': 'Database', 
+    'Parent Protein Name': 'Name', 'Assigned Protein Name': 'Title', 
+    'Assigned Protein Sequence': 'Sequence'
   })
   parent_proteins.write_csv(build_path / 'arborist' / 'parent-proteins.tsv', separator='\t')
 
 
-class EpitopeMapping:
+class EpitopeMapper:
   def __init__(self, assignments, num_threads):
     self.assignments = assignments
     self.num_threads = num_threads
     self.bin_path = Path(__file__).parents[3] / 'bin'
 
   def make_epitope_mapping(self):
-    exact_match_df, non_exact_match_df, discontinuous_df = self.split_peptide_assignments()
-    exact_mappings = self.calculate_exact_mappings(exact_match_df)
-    non_exact_mappings = self.blast_non_exact_matches(non_exact_match_df)
+    exact_matches, non_exact_linear, non_exact_discontinuous = self.split_assignments()
+    exact_mappings = self.calculate_exact_mappings(exact_matches)
+    linear_mappings = self.blast_linear_peptides(non_exact_linear)
+    discontinous_mappings = self.map_discontinuous_peptides(non_exact_discontinuous)
+
     
-  def split_peptide_assignments(self):
-    exact_match_df = self.assignments.filter(
+  def split_assignments(self):
+    exact_matches = self.assignments.filter(
       (pl.col('Assigned Protein Starting Position').is_not_null()) & 
       (pl.col('Assigned Protein ID').is_not_null())
     )
-    non_exact_match_df = self.assignments.filter(
+    non_exact_linear = self.assignments.filter(
       (pl.col('Assigned Protein Starting Position').is_null()) & 
       (pl.col('Assigned Protein ID').is_not_null()) &
       (~pl.col('Epitope Sequence').str.contains('0|1|2|3|4|5|6|7|8|9')) &
       (pl.col('Epitope ID').is_not_null())
     )
-    discontinuous_df = self.assignments.filter(
+    non_exact_discontinuous = self.assignments.filter(
       (pl.col('Assigned Protein Starting Position').is_null()) & 
       (pl.col('Assigned Protein ID').is_not_null()) &
       (pl.col('Epitope Sequence').str.contains('0|1|2|3|4|5|6|7|8|9')) &
       (pl.col('Epitope ID').is_not_null())
     )
-    return exact_match_df, non_exact_match_df, discontinuous_df
+    return exact_matches, non_exact_linear, non_exact_discontinuous
   
-  def calculate_exact_mappings(self, exact_match_df):
+  def calculate_exact_mappings(self, exact_matches):
     pass
 
-  def blast_non_exact_matches(self, non_exact_match_df):
+  def blast_linear_peptides(self, non_exact_linear):
     blast_temp = build_path / 'blast_temp'
     blast_temp.mkdir(exist_ok=True)
-    self.make_blast_db(non_exact_match_df, blast_temp)
-    self.make_epitope_file(non_exact_match_df, blast_temp)
+    self.make_blast_db(non_exact_linear, blast_temp)
+    self.make_epitope_file(non_exact_linear, blast_temp)
     cmd = [
       str(self.bin_path / 'blastp'),
       '-query', str(blast_temp / 'epitopes.fasta'),
       '-db', str(blast_temp / 'proteins.fasta'),
-      '-outfmt', '10',
+      '-outfmt', '10 qseqid sseqid qseq sseq pident length gaps qstart qend sstart send evalue',
       '-num_threads', str(self.num_threads),
       '-out', str(blast_temp / 'blast_results.csv')
     ]
     subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return self.calculate_blast_mappings(blast_temp)
 
-  def make_blast_db(self, non_exact_match_df, blast_temp):
+  def make_blast_db(self, non_exact_linear, blast_temp):
+    proteins = non_exact_linear.unique(subset=['Assigned Protein ID'])
     with open(str(blast_temp / 'proteins.fasta'), 'w') as f:
-      for row in non_exact_match_df.iter_rows(named=True):
+      for row in proteins.iter_rows(named=True):
         f.write(f'>{row["Assigned Protein ID"]}\n{row["Assigned Protein Sequence"]}\n')
     cmd = [
       str(self.bin_path / 'makeblastdb'),
@@ -140,19 +144,22 @@ class EpitopeMapping:
     ]
     subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-  def make_epitope_file(self, non_exact_match_df, blast_temp):
+  def make_epitope_file(self, non_exact_linear, blast_temp):
     with open(str(blast_temp / 'epitopes.fasta'), 'w') as f:
-      for row in non_exact_match_df.iter_rows(named=True):
+      for row in non_exact_linear.iter_rows(named=True):
         f.write(f'>{int(row["Epitope ID"])}\n{row["Epitope Sequence"]}\n')
 
   def calculate_blast_mappings(self, blast_temp):
     blast_cols = [
-      'Query', 'Subject', '% Identity', 'Alignment Length', 'Mismatches', 'Gap Openings', 
-      'Query Start', 'Query End', 'Subject Start', 'Subject End', 'E-value', 'Bit Score'
+      'Query', 'Subject', 'Query Sequence', 'Subject Sequence', '% Identity', ' Alignment Length',
+      'Gaps', 'Query Start', 'Query End', 'Subject Start', 'Subject End', 'E-value'
     ]
     blast_results = pl.read_csv(
       blast_temp / 'blast_results.csv', separator=',', has_header=False, new_columns=blast_cols
     )
+
+  def map_discontinuous_peptides(self, non_exact_discontinuous):
+    pass
 
 
 if __name__ == '__main__':
@@ -165,9 +172,9 @@ if __name__ == '__main__':
   source_data = pl.read_csv(build_path / 'arborist' / 'all-source-data.tsv', separator='\t')
   species_data = pl.read_csv(build_path / 'arborist' / 'all-species-data.tsv', separator='\t')
 
-  # all_parent_data = get_all_parent_data(assignments, source_data, species_data)
-  # make_source_parents(all_parent_data)
-  # make_parent_proteins(all_parent_data)
+  all_parent_data = get_all_parent_data(assignments, source_data, species_data)
+  make_source_parents(all_parent_data)
+  make_parent_proteins(all_parent_data)
 
-  epitope_mapping = EpitopeMapping(assignments, args.num_threads)
+  epitope_mapping = EpitopeMapper(assignments, args.num_threads)
   epitope_mapping.make_epitope_mapping()
