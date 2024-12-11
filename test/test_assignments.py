de9416e32ee9f5c99adc6e3f0aa90e2a69deeb12
Daniel Marrama <dmarrama@lji.org>
2024-12-11 13:44:49 -0800

    compare assignment test complete for first use case

---
+++ test/test_assignments.py
@@ -0,0 +1,49 @@
+import pytest
+import polars as pl
+from pathlib import Path
+
+species = [108]
+
+@pytest.fixture
+def species_path() -> Path:
+  return Path(__file__).parent / 'build' / 'species'
+
+@pytest.fixture(autouse=True)
+def cleanup_files(species_path):
+    # This runs before each test
+    yield
+    # This runs after each test
+    files_to_remove = [
+        'peptide-assignments.tsv',
+        'proteome.db',
+        'source-data.tsv',
+        'peptide-matches.tsv'
+    ]
+    
+    for taxon in species:
+      species_dir = species_path / str(taxon)
+      for file in files_to_remove:
+        file_path = species_dir / file
+        if file_path.exists():
+            file_path.unlink()
+
+def test_assignment_output(species_path):
+  for taxon in species:
+    species_dir = species_path / str(taxon)
+    
+    generated_path = species_dir / 'peptide-assignments.tsv'
+    print(generated_path)
+    assert generated_path.exists(), f"Generated assignments file not found for species {taxon}"
+    generated_df = pl.read_csv(generated_path, separator='\t')
+    
+    expected_path = species_dir / 'expected-assignments.tsv'
+    assert expected_path.exists(), f"Expected assignments file not found for species {taxon}"
+    expected_df = pl.read_csv(expected_path, separator='\t')
+    
+    generated_df = generated_df.sort(generated_df.columns)
+    expected_df = expected_df.sort(expected_df.columns)
+    
+    assert generated_df.columns == expected_df.columns, \
+        f"Column mismatch for species {taxon}"
+    
+    assert generated_df.equals(expected_df)
